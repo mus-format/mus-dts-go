b8c8de1a2da39c25643ce533c5e77bd1865364f2
yra <ymz.ncnk@outlook.com>
2024-05-09 00:46:45 +0300

    Correct docs

--- mus-dts.go
+++ mus-dts.go
@@ -11,32 +11,32 @@ func New[T any](dtm com.DTM, m mus.Marshaller[T], u mus.Unmarshaller[T],
 	return DTS[T]{dtm, m, u, s}
 }
 
-// DTS provides data type metadata support for the mus-go serializer.
+// DTS provides data type metadata (DTM) support for the mus-go serializer. It
+// helps to encode DTM + data.
 //
-// It implements mus.Marshaller, mus.Unmarshaller and mus.Sizer interfaces.
+// Implements mus.Marshaller, mus.Unmarshaller and mus.Sizer interfaces.
 type DTS[T any] struct {
 	dtm com.DTM
-
-	m mus.Marshaller[T]
-	u mus.Unmarshaller[T]
-	s mus.Sizer[T]
+	m   mus.Marshaller[T]
+	u   mus.Unmarshaller[T]
+	s   mus.Sizer[T]
 }
 
-// DTM returns a data type metadata.
+// DTM returns the value with which DTS was initialized.
 func (dts DTS[T]) DTM() com.DTM {
 	return dts.dtm
 }
 
-// MarshalMUS marshals DTM and data to the MUS format.
+// MarshalMUS marshals DTM + data.
 func (dts DTS[T]) MarshalMUS(t T, bs []byte) (n int) {
 	n = MarshalDTM(dts.dtm, bs)
 	n += dts.m.MarshalMUS(t, bs[n:])
 	return
 }
 
-// UnmarshalMUS unmarshals DTM and data from the MUS format.
+// UnmarshalMUS unmarshals DTM + data.
 //
-// Returns ErrWrongDTM if DTM from bs is different.
+// Returns ErrWrongDTM if the unmarshalled DTM differs from the dts.DTM().
 func (dts DTS[T]) UnmarshalMUS(bs []byte) (t T, n int, err error) {
 	dtm, n, err := UnmarshalDTM(bs)
 	if err != nil {
@@ -52,13 +52,13 @@ func (dts DTS[T]) UnmarshalMUS(bs []byte) (t T, n int, err error) {
 	return
 }
 
-// SizeMUS calculates the DTM and data size in the MUS format.
+// SizeMUS calculates the size of the DTM + data.
 func (dts DTS[T]) SizeMUS(t T) (size int) {
 	size = SizeDTM(dts.dtm)
 	return size + dts.s.SizeMUS(t)
 }
 
-// UnmarshalMUS unmarshals data without DTM from the MUS format.
+// UnmarshalData unmarshals only data.
 func (dts DTS[T]) UnmarshalData(bs []byte) (t T, n int, err error) {
 	return dts.u.UnmarshalMUS(bs)
 }
